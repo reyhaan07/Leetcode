54329c4f8c2276874fbbd1d9f67e45f4efd4464a
Reyhaan <rsvalor.2007@gmail.com>
2026-07-29 10:34:17 +0530

    Time: 33 ms (91.19%), Space: 151.3 MB (46.45%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,28 @@
+class MinStack {
+    stack<int> st, mn;
+
+public:
+    MinStack() {
+    }
+
+    void push(int val) {
+        st.push(val);
+        if (mn.empty())
+            mn.push(val);
+        else
+            mn.push(min(val, mn.top()));
+    }
+
+    void pop() {
+        st.pop();
+        mn.pop();
+    }
+
+    int top() {
+        return st.top();
+    }
+
+    int getMin() {
+        return mn.top();
+    }
+};
